adff83755087b0fec290122a24fa439098f266e0
Greg Osetrov <shadow-greg@yandex.ru>
2023-11-28 09:56:38 +0300

    add Message.cs

---
+++ ConsoleApp1/Message.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp1; 
+
+public class Message {
+    public string Text { get; set; }
+    public string Author { get; set; }
+    public string Transmitter { get; set; }
+    public DateTime Date { get; set; }
+    public int Id { get; set; }
+    
+    public string SerializeMessageToJson() {
+        return JsonSerializer.Serialize(this);
+    }
+    public static Message? DeserializeMessageFromJson(string json) {
+        return JsonSerializer.Deserialize<Message>(json);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using ConsoleApp1;
+
+// Create a new message.
+Message message = new Message();
+message.Text = "This is a message.";
+message.Author = "John Doe";
+message.Transmitter = "Jane Doe";
+message.Date = DateTime.Now;
+message.Id = 1;
+
+// Serialize the message to JSON.
+string json = message.SerializeMessageToJson();
+
+// Deserialize the message from JSON.
+Message deserializedMessage = Message.DeserializeMessageFromJson(json);
+
+// Print the message.
+Console.WriteLine(deserializedMessage.Text);
